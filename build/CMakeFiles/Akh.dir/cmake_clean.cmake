93d5357003b4b1d211188ced419bb2bc3fced328
Eldarushkaa <naruto3337@gmail.com>
2023-12-04 23:17:31 +0300

    cmake

---
+++ build/CMakeFiles/Akh.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Akh.exe"
+  "Akh.exe.manifest"
+  "Akh.pdb"
+  "CMakeFiles/Akh.dir/main.cpp.obj"
+  "CMakeFiles/Akh.dir/main.cpp.obj.d"
+  "libAkh.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Akh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
