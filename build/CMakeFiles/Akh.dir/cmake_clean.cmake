be9d9ce3bb2e0455390f03dbcfa460308528af9a
Eldarushkaa <naruto3337@gmail.com>
2023-12-06 01:00:57 +0300

    bwt

--- build/CMakeFiles/Akh.dir/cmake_clean.cmake
+++ build/CMakeFiles/Akh.dir/cmake_clean.cmake
@@ -2,12 +2,14 @@ file(REMOVE_RECURSE
   "Akh.exe"
   "Akh.exe.manifest"
   "Akh.pdb"
+  "CMakeFiles/Akh.dir/libraries/libsais.c.obj"
+  "CMakeFiles/Akh.dir/libraries/libsais.c.obj.d"
   "CMakeFiles/Akh.dir/main.cpp.obj"
   "CMakeFiles/Akh.dir/main.cpp.obj.d"
   "libAkh.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/Akh.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
