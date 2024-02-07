8a06cb68a97a39f81362b559b536381db26b3221
El's Mac <Naruto3337@gmail.com>
2024-02-07 20:13:31 +0700

    almost there

--- algorithems.h
+++ algorithems.h
@@ -25,8 +25,16 @@ int32_t mtf(uint8_t * T, uint8_t * U, int32_t n, uint8_t * alphabet);
 */
 int32_t reverse_mtf(uint8_t * T, uint8_t * U, int32_t n, uint8_t * alphabet);
 
-int32_t huffman(uint8_t * T, std::vector<bool>& U, int32_t n, int32_t * freq);
+int32_t huffman(uint8_t * T, std::vector<bool>& U, int32_t n, int32_t * freq, std::vector<std::vector<bool> >& symb_code);
 
 int32_t huffman_dict_to_string(std::vector<std::vector<bool> >& symb_code, std::string& output_dict);
 
 int32_t binary_vector_to_string(std::vector<bool>& binary, std::string& output);
+
+int32_t string_to_binary_vector(std::vector<bool>& binary, std::string& output);
+
+int32_t huffman_string_to_dict(std::vector<std::vector<bool> >& symb_code, std::string& output_dict);
+
+int32_t compressed_string(std::string& huffman_code, std::vector<bool>& U, std::string& compressed_data);
+
+int32_t decompressed_string(std::string& huffman_code, std::vector<bool>& U, std::string& compressed_data);
--- main.cpp
+++ main.cpp
@@ -2,107 +2,60 @@
 #include "libraries/libsais.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <map>
 #include <string>
 #include <vector>
 
 
 int main(){
-    // int weight[0x200];
-    // int count = 0;
-    // for (auto &i: weight) i = 0;
-    // std::ifstream f("C:\\Users\\FlexStreet\\Documents\\VS Code projects\\akhzip\\main.cpp");
-    // while (!f.eof())
-    // {
-    //     unsigned char ch;
-    //     f.read((char *)&ch, sizeof(ch));
-    //     count++;
-    //     weight[ch]++;
-    // }
-    // std::multimap<int, char> sorted_weight;
-    // for (int i = 0; i < 0x100; i++)
-    //     sorted_weight.insert(std::make_pair(weight[i], (char)i));
-    // for (auto i: sorted_weight)
-    //     if (i.first > 0)
-    //         std::cout << i.first << " " << i.second << std::endl;
-    // std::cout << "count: " << count << std::endl;
-    // std::ifstream is;
-    // is.open("C:\\Users\\FlexStreet\\Documents\\VS Code projects\\akhzip\\image.data");
-    // is.seekg(0, std::ios::end);
-    // int length = is.tellg();
-    // is.seekg(0, std::ios::beg);
-    // char* buffer = new char [20000];
-    // buffer[19999] = '\0';
-    // is.read(buffer, 19999);
-    // std::cout << "\nlength of image.data: " << length << std::endl;
-    // std::multimap<int, char> sorted_weight;
-    // int weight[0x100];
-    // for (auto &i: weight) 
-    //     i = 0;
-    // for (int i = 0; i < 19999; i++) 
-    //     weight[buffer[i] + 128]++;
-    // for (int i = 0; i < 0x100; i++)
-    //     sorted_weight.insert(std::make_pair(weight[i], (char)i));
-    // for (auto i: sorted_weight)
-    //     std::cout << i.first << " " << i.second <<std::endl;
-    
-    // buffer = bwt(buffer, 10);
-    // std::cout << buffer << std::endl;
-    std::string file_name = "../enwik8";
-    std::ifstream is(file_name, std::ios::binary);
-    if (!is.is_open()){
-        std::cout << "Sry no " << file_name << std::endl;
-        return 0;
-    }
     int32_t freq[256];
+    std::string DATA_FILE_NAME = "../huffman_test.txt";
+    
+    std::ifstream t(DATA_FILE_NAME);
+    std::stringstream buffer;
+    buffer << t.rdbuf();
 
-    int32_t length = 400000;
-    std::vector<uint8_t> buffer(length);
-    is.read(reinterpret_cast<char*>(&buffer.front()), buffer.size()*sizeof(uint8_t));
-    is.close();
-    std::cout << "buffer size = " << buffer.size() << std::endl;
-    uint8_t lesgo[length];
-    for (int i = 0; i < length; i++) lesgo[i] = buffer[i];
-    // std::cout << lesgo << std::endl;
-    // for (int i = length - 4; i < length; i++)
-    //     std::cout << "!i = " << i << " lesgo[i] = " << (int)lesgo[i] << std::endl;
+    int32_t length = buffer.str().size();
+    std::cout << "Data from " << DATA_FILE_NAME << " has been red. Size is " << length << " bytes" << std::endl;
+    uint8_t lesgo[length + 1];
+    lesgo[length] = 0;
+    for (int i = 0; i < length; i++) lesgo[i] = buffer.str()[i];
+
+    if (length < 100) std::cout << "ORIGINAL TEXT: " << lesgo << std::endl;
+
     int32_t extraspace[length];
     libsais_bwt(lesgo, lesgo, extraspace, length, 0, NULL);
-    // for (int i = 0; i < 1000; i++) std::cout << "lesgo[" << i << "] = (" << (int)lesgo[i] << ") '" << (char)lesgo[i] << "'" << std::endl;
-    // std::cout << lesgo << std::endl;
-    // for (int i = 0; i < 1000; i++) std::cout << lesgo[i];
-    // std::cout << std::endl;
+
+    if (length < 100) std::cout << "LIBSAIS TEXT: " << lesgo << std::endl;
     
     uint8_t alphabet[0x100];
-    std::cout << "libsasisi works" << std::endl;
+    
     for (int i = 0; i < 0x100; i++) alphabet[i] = i;
     mtf(lesgo, lesgo, length, alphabet);
-    // for (int i = 0; i < 1000; i++) std::cout << (int)lesgo[i] << " ";
-    // std::cout << std::endl;
+
+    if (length < 100){
+        std::cout << "MTF TEXT: ";
+        for (int i = 0; i < length; i++) std::cout << lesgo[i] << "(" << (int) lesgo[i] << ")";
+        std::cout << std::endl;
+    }
 
     for (int i = 0; i < 0x100; i++) freq[i] = 0;
     for (int i = 0; i < length; i++) freq[lesgo[i]]++;
-    for (int i = 0; i < 0x100; i++) std::cout << "freq[" << i << "] = " << freq[i] << std::endl;
 
     std::vector<bool> U;
-    huffman(lesgo, U, length, freq);
-    std::cout <<"Okay lesgo length * 8 = " << length * 8 << " U.size() = " << U.size() << " ratio = " << (float)U.size() / length / 8 << std::endl;
+    std::vector<std::vector<bool> > symb_code(0x100);
+    huffman(lesgo, U, length, freq, symb_code);
+    std::cout <<"Okay lesgo length * 8 = " << length * 8 << " U.size() = " << U.size() << ". Compression ratio = " << (float)U.size() / length / 8 << std::endl;
 
-    // uint8_t test[8];
-    // uint8_t mtf_test[8];
+    std::string huffman_code;
+    huffman_dict_to_string(symb_code, huffman_code);
 
-    // std::string to_test = "bananaaa";
-    // for (int i = 0; i < 8; i++) test[i] = to_test[i];
-    
-    // for (int i = 0; i < 0x100; i++) alphabet[i] = i;
-    // mtf(test, test, 8, alphabet);
-    // for (int i = 0; i < 8; i++) std::cout << (int)test[i] << std::endl;
-    // // for (int i = 0; i < 0x100; i++) std::cout << alphabet[i] << " ";
-    // std::cout << std::endl;
+    std::string compressed_data;
+    compressed_string(huffman_code, U, compressed_data);
+    std::vector<bool> U2;
+    decompressed_string(huffman_code, U2, compressed_data);
+    std::cout << "U2.size() = " << U2.size() << std::endl;
 
-    // for (int i = 0; i < 0x100; i++) alphabet[i] = i;
-    // reverse_mtf(test, test, 8, alphabet);
-    // for (int i = 0; i < 8; i++) std::cout << test[i] << std::endl;
-    // // for (int i = 0; i < 0x100; i++) std::cout << alphabet[i] << " ";
     return 0;
 }
